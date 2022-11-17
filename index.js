cfe9669258ec4f5406d8d2cec41d274f88f88efa
Kristina Vlasovets <kristinavlasovets@gmail.com>
2022-11-17 21:21:12 +0300

    added cors

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const authRouter = require("./authRouter");
+const express = require('express');
+const mongoose = require('mongoose');
+const authRouter = require('./authRouter');
 const PORT = process.env.PORT || 5000;
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
-app.use("/auth", authRouter);
+app.use(cors());
+app.use('/auth', authRouter);
 
 const start = async () => {
-  try {
-    await mongoose.connect(
-      `mongodb+srv://qwerty:qwerty123@cluster0.nkhtenn.mongodb.net/reg-auth?retryWrites=true&w=majority`
-    );
-    app.listen(PORT, () => console.log(`server started on post ${PORT}`));
-  } catch (e) {
-    console.log(e);
-  }
+	try {
+		await mongoose.connect(
+			`mongodb+srv://qwerty:qwerty123@cluster0.nkhtenn.mongodb.net/reg-auth?retryWrites=true&w=majority`
+		);
+		app.listen(PORT, () => console.log(`server started on post ${PORT}`));
+	} catch (e) {
+		console.log(e);
+	}
 };
 
 start();
