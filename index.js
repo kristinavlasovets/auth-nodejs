a7a6f6b7e16eb554c6b0c298dd27964c98a6d966
kristinavlasovets <61822704+kristinavlasovets@users.noreply.github.com>
2022-11-21 11:25:11 +0300

    added dotenv

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const authRouter = require('./authRouter');
 const PORT = process.env.PORT || 5000;
 const cors = require('cors');
+require('dotenv').config()
 
 const app = express();
 
